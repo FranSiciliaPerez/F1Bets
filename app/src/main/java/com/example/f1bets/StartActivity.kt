e39f55e6a7cfc66571b34b94ee11bbf2009893b7
Francisco Javier Sicilia Pérez <franjsp23@gmail.com>
2024-01-16 17:17:53 +0100

    Probando actividades

---
+++ app/src/main/java/com/example/f1bets/StartActivity.kt
@@ -0,0 +1,22 @@
+package com.example.f1bets
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.f1bets.databinding.ActivityStartBinding
+
+class StartActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityStartBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityStartBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // Asegúrate de que el botón esté inicializado correctamente
+        binding.button1.setOnClickListener {
+            startActivity(Intent(this, MainActivity::class.java))
+        }
+    }
+}
