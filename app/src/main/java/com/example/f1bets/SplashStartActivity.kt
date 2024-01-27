d91b2216d0cbb806d1684027a6d88d6bcb671c4c
Francisco Javier Sicilia Pérez <franjsp23@gmail.com>
2024-01-27 17:10:26 +0100

    Probando login inicial

--- app/src/main/java/com/example/f1bets/SplashStartActivity.kt
+++ app/src/main/java/com/example/f1bets/SplashStartActivity.kt
@@ -3,10 +3,12 @@ package com.example.f1bets
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.FirebaseApp
 
 class SplashStartActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+         FirebaseApp.initializeApp(this);
          startActivity(Intent(this, StartActivity::class.java))
         finish()//para evitar que pulsando el boton de atrás buelva a verse el splash
     }
--- app/src/main/java/com/example/f1bets/StartActivity.kt
+++ app/src/main/java/com/example/f1bets/StartActivity.kt
@@ -3,20 +3,45 @@ package com.example.f1bets
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.f1bets.databinding.ActivityStartBinding
+import android.widget.Button
+import com.google.android.material.snackbar.Snackbar
+import com.google.android.material.textfield.TextInputEditText
+import com.google.firebase.FirebaseApp
+import com.google.firebase.auth.FirebaseAuth
 
 class StartActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityStartBinding
+    private lateinit var firebaseAuth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_start)
 
-        binding = ActivityStartBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        firebaseAuth = FirebaseAuth.getInstance()
 
-        // Asegúrate de que el botón esté inicializado correctamente
-        binding.btnLogin.setOnClickListener {
-            startActivity(Intent(this, MainActivity::class.java))
+        val btnLogin: Button = findViewById(R.id.btnLogin)
+        val emailEditText: TextInputEditText = findViewById(R.id.email)
+        val passwordEditText: TextInputEditText = findViewById(R.id.password)
+
+        btnLogin.setOnClickListener {
+            val email = emailEditText.text.toString()
+            val password = passwordEditText.text.toString()
+
+            signIn(email, password)
         }
+    }
+
+    private fun signIn(email: String, password: String) {
+        firebaseAuth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Autenticación exitosa, redirige a MainActivity
+                    val intent = Intent(this, MainActivity::class.java)
+                    startActivity(intent)
+                    finish() // Cierra la actividad actual para que no pueda volver atrás con el botón de retroceso
+                } else {
+                    // Mostrar mensaje de error en caso de fallo
+                    Snackbar.make(findViewById(android.R.id.content), "Inicio de sesión fallido", Snackbar.LENGTH_LONG).show()
+                }
+            }
     }
 }
