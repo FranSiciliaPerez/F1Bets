409135e0a0e3e2163064e4f443f35aa6531795e4
Francisco Javier Sicilia Pérez <franjsp23@gmail.com>
2024-02-19 13:26:50 +0100

    Añado un audio que suena cada vez que aparece el splash con el logo al entrar en la app

--- app/src/main/java/com/example/f1bets/SplashStartActivity.kt
+++ app/src/main/java/com/example/f1bets/SplashStartActivity.kt
@@ -1,17 +1,34 @@
 package com.example.f1bets
 
 import android.content.Intent
+import android.media.MediaPlayer
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.FirebaseApp
 
 class SplashStartActivity : AppCompatActivity() {
+
+    private lateinit var mediaPlayer: MediaPlayer
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
          FirebaseApp.initializeApp(this);
-         startActivity(Intent(this, StartActivity::class.java))
-        finish()// Evita que pulsando el botón de atrás aparezca otra vez el splash
+
+        // Inicializa el MediaPlayer con el archivo de audio
+        mediaPlayer = MediaPlayer.create(this, R.raw.f1)
+        // Reproduce el audio
+        mediaPlayer.start()
+
+        // Inicia la actividad principal después de un breve retraso
+        mediaPlayer.setOnCompletionListener {
+            startActivity(Intent(this, StartActivity::class.java))
+            finish() // Evitar que pulsar el botón de atrás vuelva a mostrar la pantalla de Splash
+        }
     }
 
-
+    override fun onDestroy() {
+        super.onDestroy()
+        // Libera los recursos del MediaPlayer cuando la actividad se destruye
+        mediaPlayer.release()
+    }
 }
