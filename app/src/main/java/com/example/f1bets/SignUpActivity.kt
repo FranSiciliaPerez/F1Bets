ba47ab070bb21fc08c04545bd15941327c401a86
Francisco Javier Sicilia Pérez <franjsp23@gmail.com>
2024-02-19 11:06:15 +0100

    Añadido permiso de internet(se me olvidó ponerlo antes), login casi terminado

--- app/src/main/java/com/example/f1bets/SignUpActivity.kt
+++ app/src/main/java/com/example/f1bets/SignUpActivity.kt
@@ -4,13 +4,15 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.f1bets.databinding.ActivitySignUpBinding
+import com.example.f1bets.entities.User
 import com.google.android.material.snackbar.Snackbar
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
 
 class SignUpActivity : AppCompatActivity() {
     private lateinit var binding: ActivitySignUpBinding
     private lateinit var auth: FirebaseAuth
-    //private lateinit var db: FirebaseFirestore -> Próximamente
+    private lateinit var db: FirebaseFirestore
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,63 +21,48 @@ class SignUpActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         auth = FirebaseAuth.getInstance()
+        db = FirebaseFirestore.getInstance()
 
         with(binding) {
             btnSignUp.setOnClickListener {
-                val user = binding.textUser.text.toString()
-                val email = binding.textEmail.text.toString()
-                val password = binding.txtPassword2.text.toString()
+                val user = textUser.text.toString()
+                val email = textEmail.text.toString()
+                val password = txtPassword2.text.toString()
 
-                // Comprobamos si todos los campos tienen información llamando a la clase funciones
                 if (Funciones.allFilled(user, email, password)) {
-                    if (checkBox.isChecked){
-                        auth
-                            .createUserWithEmailAndPassword(email, password)
-                            .addOnSuccessListener { /*task ->*/
-                                    // If login is successful, redirect to the main activity
-                                    val i = Intent(this@SignUpActivity, MainActivity::class.java)
-                                    i.putExtra((R.string.succesLog.toString()), (R.string.succesLog.toString()))
-                                    startActivity(i)
-                                    finish()
-                                /* Guarda información adicional en Firestore
-                                val authResult = task
-                                saveAdditionalUserData(authResult?.user?.uid, user)*/
-
+                    if (checkBox.isChecked) {
+                        auth.createUserWithEmailAndPassword(email, password)
+                            .addOnSuccessListener { authResult ->
+                                // Si el registro es exitoso, guarda información adicional en Firestore
+                                val newUser =
+                                    User(authResult.user?.uid ?: "", user, email, "")
+                                saveAdditionalUserData(newUser)
                             }
-                            .addOnFailureListener{
+                            .addOnFailureListener {
                                 Snackbar.make(root, R.string.errEmail, Snackbar.LENGTH_LONG).show()
                             }
                     } else {
-                        // Comprobamos si está marcado o no el checkBox para continuar ya que es obligatorio
                         Snackbar.make(root, R.string.msgCheckError, Snackbar.LENGTH_LONG).show()
                     }
                 } else {
-                    // Algún campo está vacío: mostramos error
                     Snackbar.make(root, R.string.errEmpty, Snackbar.LENGTH_LONG).show()
                 }
             }
         }
     }
-    /*private fun saveAdditionalUserData(userId: String?, username: String) {
 
-        val user = hashMapOf(
-            "userId" to userId,
-            "username" to username,
-            "profileImageUri" to "" // Guardo la URL de la imagen para que la vea despues Firebase Storage
-        )
-
-        if (userId != null) {
-            db.collection("users")
-                .document(userId)
-                .set(user)
-                .addOnSuccessListener {
-                    // Éxito al guardar datos adicionales en Firestore
-                    Snackbar.make(binding.root, R.string.succesLog, Snackbar.LENGTH_LONG).show()
-                }
-                .addOnFailureListener {
-                    // Muestra error en Firestore al almacenar los campos adicionales
-                    Snackbar.make(binding.root, R.string.errSignFirestore, Snackbar.LENGTH_LONG).show()
-                }
-        }
-    }*/
+    private fun saveAdditionalUserData(user: User) {
+        db.collection("user")
+            .document(user.uid)
+            .set(user)
+            .addOnSuccessListener {
+                Snackbar.make(binding.root, R.string.succesLog, Snackbar.LENGTH_LONG).show()
+                val i = Intent(this@SignUpActivity, MainActivity::class.java)
+                startActivity(i)
+                finish()
+            }
+            .addOnFailureListener {
+                Snackbar.make(binding.root, R.string.errSignFirestore, Snackbar.LENGTH_LONG).show()
+            }
+    }
 }
